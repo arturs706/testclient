f3619ad5c781e8c16b241d0e397a4e9bc2f251d2
Arturs Radionovs <artursradionovs@Arturss-MacBook-Air.local>
2023-03-07 15:53:30 +0000

    first-commit

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react'
 
 
 export default function Home() {
+  const [fontSizeH1, setFontSizeH1] = useState(0)
   const [widthSize, setWidthSize] = useState(0);
   //create a function that assigns the screen width to the state so it can be returned
   const handleResize = () => {
@@ -22,11 +23,17 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      <div className={styles.maindiv}>
-        <h1>{widthSize}</h1>
-      </div>
-      <div className={styles.maindiv}>
-      </div>
+      <section className={styles.sectionone}>
+        <div className={styles.divone}>
+          <div className={styles.maindivh1} style={{ fontSize: `${fontSizeH1}px` }}><h1>Samsung<br></br>S23 Series</h1></div>
+        </div>
+        <div className={styles.divtwo}><h1>{widthSize}</h1></div>
+        <div className={styles.divthree}>Div 3</div>
+      </section>
+
+      <div className={styles.middlediv}></div>
+
+      <section className={styles.sectiontwo}></section>
     </main>
   )
 }
